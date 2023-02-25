0599e3b2a40dbed9ddb05f4836b195a395a91ea3
Mateus Pegoraro <mateus.pegoraro@involves.com>
2023-02-25 09:49:41 -0300

    Criação de transacões

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,33 @@
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
+  app.post('/', async (request, response) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions')
+      .insert({
+        id: randomUUID(),
+        session_id: randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+      })
+      .returning('*')
+
+    return response.status(201).send()
+  })
+
+  app.get('/transactions', async () => {
     const transaction = await knex('transactions')
       .insert({
         id: randomUUID(),
